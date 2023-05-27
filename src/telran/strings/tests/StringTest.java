212ea2759de39a268a3b97652d6b0f8ae12552b6
Anatoly <anatolyavbme@gmail.com>
2023-05-27 17:46:10 +0300

    HW#1 StringTests "UniTests"

--- src/telran/strings/tests/StringTest.java
+++ src/telran/strings/tests/StringTest.java
@@ -23,9 +23,59 @@ class StringTest {
 		assertTrue(str.contains("ello"));
 		assertFalse(str.contains("lelo"));
 	}
-	@Test 
-	void compareToTest() {
-		//TODO
-	}
+	  @Test
+	    public void testCompareTo() {
+		  String str = "Hello";
+	        assertEquals(0, str.compareTo("Hello"));
+	    }
 
+    @Test
+    public void testCompareToIgnoreCase() {
+    	String str = "Hello";
+    	assertEquals(0, str.compareToIgnoreCase("Hello"));
+    }
+
+    @Test
+    public void testConcat() {
+    	String str = "Hello";
+        assertEquals("Hello World", str.concat(" World"));
+    }
+
+    @Test
+    public void testStartsWith() {
+    	String str = "Hello";
+        assertTrue(str.startsWith("Hel"));
+        assertFalse(str.startsWith("lo"));
+    }
+
+    @Test
+    public void testEndsWith() {
+    	String str = "Hello";
+        assertTrue(str.endsWith("lo"));
+        assertFalse(str.endsWith("World"));
+    }
+
+    @Test
+    public void testEqualsIgnoreCase() {
+    	String str = "Hello";
+        assertTrue(str.equalsIgnoreCase("Hello"));
+        assertFalse(str.equalsIgnoreCase("World"));
+    }
+    @Test
+    public void testIndexOf() {
+    	String str = "Hello";
+        assertEquals(0, str.indexOf("H"));
+        assertEquals(1, str.indexOf("e"));
+        assertEquals(2, str.indexOf("l"));
+        assertEquals(4, str.indexOf("o"));
+    }
+
+    @Test
+    public void testLastIndexOf() {
+    	String str = "Hello";
+        assertEquals(3, str.lastIndexOf("l"));
+        assertEquals(3, str.lastIndexOf("l"));
+        assertEquals(1, str.lastIndexOf("e"));
+        assertEquals(0, str.lastIndexOf("H"));
+    }
 }
